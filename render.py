e6ec2af94269e09dc83171f78e03fa9d087aae15
brianhouse <brian.house@gmail.com>
2019-07-02 16:02:33 -0400

    sections

--- render.py
+++ render.py
@@ -7,7 +7,7 @@ with open('README.md') as f:
     html = markdown.markdown(content, extensions=['extra', 'codehilite'])
 
 with open('README.html', 'w') as f:
-    f.write('<html><head><link rel="stylesheet" type="text/css" href="github-markdown.css" /><link rel="stylesheet" type="text/css" href="codehilite.css" /></head><body><div id="main" class="markdown-body">')
+    f.write('<!doctype html><head><meta charset="utf-8"/><link rel="stylesheet" type="text/css" href="github-markdown.css" /><link rel="stylesheet" type="text/css" href="codehilite.css" /></head><body><div id="main" class="markdown-body">')
     f.write(html)
     f.write('</div></body></html>')
 
