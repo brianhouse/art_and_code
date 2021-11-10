7c18757f2e13b548b021d32581cf110b3d2155c9
Brian House <email@brian.house>
2021-11-10 15:58:37 -0800

    helpers

--- units/7_emergence/sim_helpers.py
+++ units/7_emergence/sim_helpers.py
@@ -1,3 +1,7 @@
+def move(agent):
+    agent['x'] += cos(agent['heading']) * agent['speed']
+    agent['y'] += sin(agent['heading']) * agent['speed']
+
 def avoid(agent, other, threshold, strength):
     if other == agent:
         return 0
@@ -12,13 +16,16 @@ def avoid(agent, other, threshold, strength):
         agent['heading'] = atan2(new_velocity_y, new_velocity_x)
     return distance
 
-    
+
 def avoid_walls(agent, threshold, strength):
-    walls = [{'x': 0, 'y': agent['y']}, {'x': width, 'y': agent['y']}, {'x': agent['x'], 'y': 0}, {'x': agent['x'], 'y': height}]
-    for wall in walls:
-        avoid(agent, wall, threshold, strength)   
-    
-    
+    if agent['x'] < -threshold or agent['x'] > width + threshold or agent['y'] < -threshold or agent['y'] > height + threshold:
+        agent['heading'] = atan2(agent['y'] - height/2, agent['x'] - width/2) * 180 / PI;
+    else:
+        walls = [{'x': 0, 'y': agent['y']}, {'x': width, 'y': agent['y']}, {'x': agent['x'], 'y': 0}, {'x': agent['x'], 'y': height}]
+        for wall in walls:
+            avoid(agent, wall, threshold, strength)
+
+
 def seek(agent, other, threshold, strength):
     if other == agent:
         return 0
@@ -32,4 +39,14 @@ def seek(agent, other, threshold, strength):
         new_velocity_y = velocity_y + steer_y
         agent['heading'] = atan2(new_velocity_y, new_velocity_x)
     return distance
-    
+
+
+def position(agent, draw_f):
+    pushMatrix()
+    translate(agent['x'], agent['y'])
+    rotate(agent['heading'])
+    draw_f(agent)
+    popMatrix()
+
+def step_cycle(n, rate):
+    return (frameCount / int(rate)) % n
